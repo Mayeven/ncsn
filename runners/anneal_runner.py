d09faed3a7d268480f36e5cc505adb1490dce037
Mayeven <120073879+Mayeven@users.noreply.github.com>
2024-09-25 17:15:56 +0800

    Update anneal_runner.py
    The code im = Image.fromarray(image_grid.mul_(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to('cpu', torch.uint8).numpy() modifies the values of image_grid itself, which causes issues with the line save_image(image_grid, os.path.join(self.args.image_folder, 'image_completion_{}.png'.format(i))). The saved images have problems at multiples of 10, so it's recommended to place this code beforehand.

--- runners/anneal_runner.py
+++ runners/anneal_runner.py
@@ -248,12 +248,13 @@ class AnnealRunner():
                     sample = torch.sigmoid(sample)
 
                 image_grid = make_grid(sample, nrow=grid_size)
-                if i % 10 == 0:
-                    im = Image.fromarray(image_grid.mul_(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to('cpu', torch.uint8).numpy())
-                    imgs.append(im)
 
                 save_image(image_grid, os.path.join(self.args.image_folder, 'image_{}.png'.format(i)))
                 torch.save(sample, os.path.join(self.args.image_folder, 'image_raw_{}.pth'.format(i)))
+                
+                if i % 10 == 0:
+                    im = Image.fromarray(image_grid.mul_(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to('cpu', torch.uint8).numpy())
+                    imgs.append(im)
 
 
         else:
@@ -269,12 +270,14 @@ class AnnealRunner():
                     sample = torch.sigmoid(sample)
 
                 image_grid = make_grid(sample, nrow=grid_size)
+
+                save_image(image_grid, os.path.join(self.args.image_folder, 'image_{}.png'.format(i)), nrow=10)
+                torch.save(sample, os.path.join(self.args.image_folder, 'image_raw_{}.pth'.format(i)))
+                
                 if i % 10 == 0:
                     im = Image.fromarray(image_grid.mul_(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to('cpu', torch.uint8).numpy())
                     imgs.append(im)
 
-                save_image(image_grid, os.path.join(self.args.image_folder, 'image_{}.png'.format(i)), nrow=10)
-                torch.save(sample, os.path.join(self.args.image_folder, 'image_raw_{}.pth'.format(i)))
 
         imgs[0].save(os.path.join(self.args.image_folder, "movie.gif"), save_all=True, append_images=imgs[1:], duration=1, loop=0)
 
@@ -345,12 +348,13 @@ class AnnealRunner():
                     sample = torch.sigmoid(sample)
 
                 image_grid = make_grid(sample, nrow=20)
-                if i % 10 == 0:
-                    im = Image.fromarray(image_grid.mul_(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to('cpu', torch.uint8).numpy())
-                    imgs.append(im)
 
                 save_image(image_grid, os.path.join(self.args.image_folder, 'image_completion_{}.png'.format(i)))
                 torch.save(sample, os.path.join(self.args.image_folder, 'image_completion_raw_{}.pth'.format(i)))
+                
+                if i % 10 == 0:
+                    im = Image.fromarray(image_grid.mul_(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to('cpu', torch.uint8).numpy())
+                    imgs.append(im)
 
         else:
             transform = transforms.Compose([
@@ -383,12 +387,14 @@ class AnnealRunner():
                     sample = torch.sigmoid(sample)
 
                 image_grid = make_grid(sample, nrow=20)
+
+                save_image(image_grid, os.path.join(self.args.image_folder, 'image_completion_{}.png'.format(i)))
+                torch.save(sample, os.path.join(self.args.image_folder, 'image_completion_raw_{}.pth'.format(i)))
+                
                 if i % 10 == 0:
                     im = Image.fromarray(image_grid.mul_(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to('cpu', torch.uint8).numpy())
                     imgs.append(im)
 
-                save_image(image_grid, os.path.join(self.args.image_folder, 'image_completion_{}.png'.format(i)))
-                torch.save(sample, os.path.join(self.args.image_folder, 'image_completion_raw_{}.pth'.format(i)))
 
 
         imgs[0].save(os.path.join(self.args.image_folder, "movie.gif"), save_all=True, append_images=imgs[1:], duration=1, loop=0)
